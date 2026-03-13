1aaa96e9849cffb457cf605f141b12734aa1d0ab
Chris S. W. Davis <ChrisSWDavis@gmail.com>
2026-03-13 17:33:01 +0000

    Removed setuptools, and changed to toml installation

--- AsympDirsCalculator/MAGCOSmacroFileGenerator.py
+++ AsympDirsCalculator/MAGCOSmacroFileGenerator.py
@@ -1,23 +1,13 @@
 import datetime as dt
+import importlib.resources
 
 import numpy as np
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    # Python < 3.9
-    import importlib_resources as pkg_resources
 
 from .globals import magcosRunDir
 
 class MAGCOSmacroFileGenerator():
 
-    try:
-        # For Python 3.9+
-        _MacroFileTemplateLocation = str(pkg_resources.files(__name__) / "magcos_running_scripts/AsymptoticDirection.g4mac")
-    except AttributeError:
-        # Fallback for older versions
-        import pkg_resources as old_pkg_resources
-        _MacroFileTemplateLocation = old_pkg_resources.resource_filename(__name__,"magcos_running_scripts/AsymptoticDirection.g4mac")
+    _MacroFileTemplateLocation = str(importlib.resources.files(__package__) / "magcos_running_scripts/AsymptoticDirection.g4mac")
 
     def __init__(self):
         with open(self._MacroFileTemplateLocation,"r") as templateFile:
--- AsympDirsCalculator/globals.py
+++ AsympDirsCalculator/globals.py
@@ -1,21 +1,11 @@
+import importlib.resources
 import os
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    # Python < 3.9
-    import importlib_resources as pkg_resources
 
 global _magnetocosmicsShellScriptPath, \
        originalDirectory, \
        magcosRunDir
 
-try:
-    # For Python 3.9+
-    _magnetocosmicsShellScriptPath = str(pkg_resources.files(__name__) / "magcos_running_scripts/runNoRewriteMAGCOSsimulation.sh")
-except AttributeError:
-    # Fallback for older versions
-    import pkg_resources as old_pkg_resources
-    _magnetocosmicsShellScriptPath = old_pkg_resources.resource_filename(__name__,"magcos_running_scripts/runNoRewriteMAGCOSsimulation.sh")
+_magnetocosmicsShellScriptPath = str(importlib.resources.files(__package__) / "magcos_running_scripts/runNoRewriteMAGCOSsimulation.sh")
 
 originalDirectory = os.getcwd()
 
